b27f27c60d186d46c62236e0c8b681294cd644cf
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 01:43:23 -0600

    agregando src/test/java/pages/tc_8Page.java

---
+++ src/test/java/pages/tc_8Page.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc_8Page {
+    WebDriver driver;
+
+    public tc_8Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void performActionH() {
+        // Implement the actions needed to execute action H
+    }
+
+    public void verifyResultH() {
+        // Implement the verification of result H
+    }
+}
