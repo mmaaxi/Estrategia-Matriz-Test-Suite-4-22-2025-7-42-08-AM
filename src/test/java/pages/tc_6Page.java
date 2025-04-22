e2d3fbaec103a43f9954bf130bf65660d5056247
Maximiliano Reyes López <113549509+mmaaxi@users.noreply.github.com>
2025-04-22 01:43:21 -0600

    agregando src/test/java/pages/tc_6Page.java

---
+++ src/test/java/pages/tc_6Page.java
@@ -0,0 +1,19 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class tc_6Page {
+    WebDriver driver;
+
+    public tc_6Page(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void performActionF() {
+        // Código para ejecutar la acción F
+    }
+
+    public void verifyResultF() {
+        // Código para verificar el resultado F
+    }
+}
